5e0fd26ed5a38f69205605a94f81c75f8a903d8d
Vincen Sanjaya <vincenciogfs15@gmail.com>
2023-05-28 07:36:25 +0700

    First COmmit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Styles{
+  static Color blue = const Color(0xFF2A63E2);
+  static Color black = const Color(0xFF111111);
+  static Color pink = const Color(0xFFF47881);
+  static Color white = const Color(0xFFFFFFFF);
+  static Color lightBlue = const Color(0xFFEFF4FD);
+  static Color lightGrey = const Color(0xFFF8F9FD);
+  static Color green = const Color(0xFF759A89);
+}
